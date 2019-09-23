dd627d8e2c3b63ef6d6c0a638db2379053db43a5
AxingG <zhaojiaxing_name@163.com>
2019-09-23 21:45:21 +0800

    bug修改

--- src/main/groovy/com/dailyyoga/filter/FileSizeTask.groovy
+++ src/main/groovy/com/dailyyoga/filter/FileSizeTask.groovy
@@ -24,38 +24,36 @@ class FileSizeTask extends DefaultTask {
                 if (resPath?.containsKey(currentProject.name)) {
                     currentResPath = resPath.get(currentProject.name)
                 }
-                def fileTree = null
                 try {
-
-                    fileTree = currentProject.fileTree(currentResPath)
-                } catch (FileNotFoundException e) {
-                    System.println(e.getMessage())
-                }
-                if (fileTree != null) {
-                    fileTree.dir.eachDir { File subFile ->
-                        def bigResource = []
-                        subFile.eachFile { File file ->
-                            def size = file.size() / 1000
-                            // 超过限制大小 && 不在白名单之中
-                            if (size > filterSize && !whiteList?.contains(file.name) && "strings.xml" != file.name) {
-                                bigResource.add(file)
+                    def fileTree = currentProject.fileTree(currentResPath)
+                    if (fileTree != null) {
+                        fileTree.dir.eachDir { File subFile ->
+                            def bigResource = []
+                            subFile.eachFile { File file ->
+                                def size = file.size() / 1000
+                                // 超过限制大小 && 不在白名单之中
+                                if (size > filterSize && !whiteList?.contains(file.name) && "strings.xml" != file.name) {
+                                    bigResource.add(file)
+                                }
                             }
-                        }
-                        if (bigResource.size() > 0) {
-                            def file = bigResource[0]
-                            def path = file.path
-                            def index = path.lastIndexOf('\\')
-                            if (index != -1) {
-                                path = path.substring(0, index)
+                            if (bigResource.size() > 0) {
+                                def file = bigResource[0]
+                                def path = file.path
+                                def index = path.lastIndexOf('\\')
+                                if (index != -1) {
+                                    path = path.substring(0, index)
+                                }
+                                def str = "${bigResource.size()}个资源文件过大\r\n文件路径:$path\r\n"
+                                bigResource.each { File bigFile ->
+                                    def size = bigFile.size() / 1000
+                                    str += "文件名：$bigFile.name 文件大小：${size}kb\r\n"
+                                }
+                                throw new GradleException(str)
                             }
-                            def str = "${bigResource.size()}个资源文件过大\r\n文件路径:$path\r\n"
-                            bigResource.each { File bigFile ->
-                                def size = bigFile.size() / 1000
-                                str += "文件名：$bigFile.name 文件大小：${size}kb\r\n"
-                            }
-                            throw new GradleException(str)
                         }
                     }
+                } catch (Throwable e) {
+                    println("文件未找到：" + currentProject.name)
                 }
             }
         }
